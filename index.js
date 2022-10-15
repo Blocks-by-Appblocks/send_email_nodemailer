15950a36f8fcf6cd8f7d9aa21ee52e43e6d89756
MohammedAshikKP <mohammed.ashik@neoito.com>
2022-10-15 17:42:43 +0530

    fix: generalized transporter object with configurable service host and port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
+import transporter from './transporter.js';
 
 const { parsed: env } = dotenv.config({ path: '../.env.functions' });
 
 const send_email_nodemailer = async (req, res) => {
     const { method, body = {} } = req;
-    const { to, subject, message } = body;
+    const { to=null, subject=null, message=null } = body;
+
 
     // health check
     if (req.params.health === 'health') {
@@ -15,47 +16,27 @@ const send_email_nodemailer = async (req, res) => {
 
     // block requests which is not a post method
     if (method !== 'POST') {
-        res.write(JSON.stringify({ success: false, msg: `Only POST method is allowed` }));
-        res.end();
+        res.status(405).send({ success: false, msg: 'Only POST method is allowed' })
+
     }
 
     if (!to || !subject || !message) {
-        res.write(JSON.stringify({ success: false, msg: `to,subject,message fields are missing in the body` }));
-        res.end();
+        res.status(400).send({ success: false, msg: 'to,subject,message fields are missing in the body'})
+
     }
-
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        secure: true,
-        auth: {
-            user: env.GMAIL_ID, // configure your credentials in .env.functions file
-            pass: env.GMAIL_PASSWORD, // configure your credentials in .env.functions file
-        },
-        tls: {
-            rejectUnauthorized: false,
-        },
-    });
-
     const mailOptions = {
         to,
-        from: env.GMAIL_ID, // configure your credentials in .env.functions file
+        from: env.EMAIL_ID, // configure your credentials in .env.functions file
         subject,
         html:message,
     };
 
     try {
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.write(JSON.stringify({ success: false, msg: error }));
-                res.end();
-            } else {
-                res.write(JSON.stringify({ success: true, msg: `Email send successfully` }));
-                res.end();
-            }
-        });
+        const response = await transporter.sendMail(mailOptions);
+        res.status(200).send({ success: true, msg: 'Message send successfully',data:response})
     } catch (error) {
-        res.write(JSON.stringify({ success: false, msg: 'Internal Error' }));
-        res.end();
+        res.status(500).send({ success: false, msg: error.message })
+        
     }
 };
 
